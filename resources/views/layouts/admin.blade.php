99d8946eb05b9d994960e9e50f0f9af62e421a29
ismail-lafhiel <lafhiel.ismail.contact@gmail.com>
2024-03-05 23:45:34 +0100

    finished emplementing Category CRUD

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -46,6 +46,8 @@
                 </x-admin-link>
                 <x-admin-link :active="request()->routeIs('admin.users.index')" href="{{ route('admin.users.index') }}">Users
                 </x-admin-link>
+                <x-admin-link :active="request()->routeIs('admin.categories.index')" href="{{ route('admin.categories.index') }}">Categories
+                </x-admin-link>
                 <div @click.away="open = false" class="relative" x-data="{ open: false }">
                     <button @click="open = !open"
                         class="flex flex-row items-center w-full px-4 py-2 mt-2 text-sm font-semibold text-left bg-transparent rounded-lg dark:bg-transparent dark:focus:text-white dark:hover:text-white dark:focus:bg-primary-600 dark:hover:bg-primary-600 md:block hover:text-primary-900 focus:text-primary-900 hover:bg-primary-200 focus:bg-primary-200 focus:outline-none focus:shadow-outline">
