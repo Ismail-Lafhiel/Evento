df0a609cfb04db6f04ada4fb05634d22ced968af
ismail-lafhiel <lafhiel.ismail.contact@gmail.com>
2024-03-06 11:37:22 +0100

    EV-13 finished integratting home/company/events/contact

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -17,7 +17,7 @@
                     <a href="#" class="hover:underline me-4 md:me-6">Licensing</a>
                 </li>
                 <li>
-                    <a href="#" class="hover:underline">Contact</a>
+                    <a href="#" class="hover:underline">Contact</a> 
                 </li>
             </ul>
         </div>
