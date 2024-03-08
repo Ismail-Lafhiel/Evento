3f01e3eb233e6a83adf06c40f792d42390769650
ismail-lafhiel <lafhiel.ismail.contact@gmail.com>
2024-03-08 16:25:53 +0100

    EV-15 category filter implemented

--- resources/views/events.blade.php
+++ resources/views/events.blade.php
@@ -8,29 +8,16 @@
                         <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Avaible
                             Events
                         </h2>
-                        <form id="searchForm" class="max-w-lg w-full justify-self-end" data-url="{{ route('search.events') }}">
+                        <form id="searchForm" class="max-w-lg w-full justify-self-end"
+                            data-url="{{ route('search.events') }}">
                             @csrf
                             <div class="flex">
-                                <button id="dropdown-button" data-dropdown-toggle="dropdown"
-                                    class="flex-shrink-0 z-10 inline-flex items-center py-2.5 px-4 text-sm font-medium text-center text-gray-900 bg-gray-100 border border-gray-300 rounded-s-lg hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700 dark:text-white dark:border-gray-600"
-                                    type="button">All categories <svg class="w-2.5 h-2.5 ms-2.5" aria-hidden="true"
-                                        xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                            stroke-width="2" d="m1 1 4 4 4-4" />
-                                    </svg>
-                                </button>
-                                <div id="dropdown"
-                                    class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
-                                    <ul class="py-2 text-sm text-gray-700 dark:text-gray-200"
-                                        aria-labelledby="dropdown-button">
-                                        @foreach ($categories as $category)
-                                            <li>
-                                                <button type="button"
-                                                    class="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">{{ $category->category_name }}</button>
-                                            </li>
-                                        @endforeach
-                                    </ul>
-                                </div>
+                                <select id="category-dropdown" name="category" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-e-lg border-s-gray-50 border-s-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-s-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500">
+                                    <option value="">All Categories</option>
+                                    @foreach ($categories as $category)
+                                        <option value="{{ $category->category_name }}" class="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">{{ $category->category_name }}</option>
+                                    @endforeach
+                                </select>
                                 <div class="relative w-full">
                                     <input type="search" id="search-dropdown" name="name"
                                         class="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-e-lg border-s-gray-50 border-s-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-s-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500"
@@ -61,7 +48,8 @@
                                     <div class="flex justify-between align-baseline">
                                         <a href="{{ route('event', $event->id) }}"
                                             class="mt-4 inline-block text-blue-200 text-sm hover:underline">Discover</a>
-                                        <span class="mt-4 inline-block text-blue-200 text-md capitalize">{{ $event->category->category_name }}</span>
+                                        <span
+                                            class="mt-4 inline-block text-blue-200 text-md capitalize">{{ $event->category->category_name }}</span>
                                     </div>
                                 </div>
                             </div>
